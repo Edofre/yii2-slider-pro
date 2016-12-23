b7d4187e14a4dcb65113ef50388442f4f0b8dcac
Edo Freriks <edofre@gmail.com>
2016-12-23 13:56:57 +0200

    Formatting spaces > tabs

--- src/models/Slide.php
+++ src/models/Slide.php
@@ -8,32 +8,32 @@ namespace edofre\sliderpro\models;
  */
 class Slide extends \yii\base\Model
 {
-	/** @var  string The string contents that will be rendered without any object interference */
-	public $content = '';
-	/** @var  array Contains objects from the /model folder, will be appended behind the $this->content string */
-	public $items = [];
+    /** @var  string The string contents that will be rendered without any object interference */
+    public $content = '';
+    /** @var  array Contains objects from the /model folder, will be appended behind the $this->content string */
+    public $items = [];
 
-	/**
-	 * @return array
-	 */
-	public function rules()
-	{
-		return [
-			[['items', 'content'], 'safe'],
-		];
-	}
+    /**
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            [['items', 'content'], 'safe'],
+        ];
+    }
 
-	/**
-	 * Append the objects after the $this->content string
-	 * @return string
-	 */
-	public function render()
-	{
-		if (empty($items)) {
-			foreach ($this->items as $item) {
-				$this->content .= $item->render();
-			}
-		}
-		return $this->content;
-	}
+    /**
+     * Append the objects after the $this->content string
+     * @return string
+     */
+    public function render()
+    {
+        if (empty($items)) {
+            foreach ($this->items as $item) {
+                $this->content .= $item->render();
+            }
+        }
+        return $this->content;
+    }
 }
--- src/models/slides/Image.php
+++ src/models/slides/Image.php
@@ -8,36 +8,36 @@ namespace edofre\sliderpro\models\slides;
  */
 class Image extends \yii\base\Model
 {
-	/** Required class for the images */
-	const IMAGE_CLASS = 'sp-image';
+    /** Required class for the images */
+    const IMAGE_CLASS = 'sp-image';
 
-	/** @var  string The source of the image */
-	public $src;
-	/** @var  array The HTML options for the layer */
-	public $htmlOptions;
+    /** @var  string The source of the image */
+    public $src;
+    /** @var  array The HTML options for the layer */
+    public $htmlOptions;
 
-	/**
-	 * @return array
-	 */
-	public function rules()
-	{
-		return [
-			[['src'], 'required'],
-			[['htmlOptions'], 'safe'],
-		];
-	}
+    /**
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            [['src'], 'required'],
+            [['htmlOptions'], 'safe'],
+        ];
+    }
 
-	/**
-	 * @return string
-	 */
-	public function render()
-	{
-		if (isset($this->htmlOptions['class'])) {
-			$this->htmlOptions['class'] = self::IMAGE_CLASS . ' ' . $this->htmlOptions['class'];
-		} else {
-			$this->htmlOptions['class'] = self::IMAGE_CLASS;
-		}
+    /**
+     * @return string
+     */
+    public function render()
+    {
+        if (isset($this->htmlOptions['class'])) {
+            $this->htmlOptions['class'] = self::IMAGE_CLASS . ' ' . $this->htmlOptions['class'];
+        } else {
+            $this->htmlOptions['class'] = self::IMAGE_CLASS;
+        }
 
-		return \yii\bootstrap\Html::img($this->src, $this->htmlOptions);
-	}
+        return \yii\bootstrap\Html::img($this->src, $this->htmlOptions);
+    }
 }
